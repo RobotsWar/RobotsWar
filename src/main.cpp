bfc0cd082a4b32dd51db065502b8b59057271e27
Gregwar <g.passault@gmail.com>
2013-02-11 15:24:03 +0100

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,7 @@
 
 volatile bool isUSB = false;
 
-TERMINAL_COMMAND(flash, "Flash the eyes")
-{
-    int i;
-    
-    for (i=0; i<50; i++) {
-        digitalWrite(EYE1, HIGH);
-        digitalWrite(EYE2, HIGH);
-        delay(50);
-        digitalWrite(EYE1, LOW);
-        digitalWrite(EYE2, LOW);
-        delay(50);
-    }
-}
-
 void setup() {
-    pinMode(EYE1, OUTPUT);
-    pinMode(EYE2, OUTPUT);
     pinMode(BOARD_LED_PIN, OUTPUT);
     servos_init();
     
