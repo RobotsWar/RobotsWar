8f511dafea9bab03ac4922519294dafed2c6632d
Gregwar <g.passault@gmail.com>
2015-09-11 16:30:42 +0200

    Maple mini support for robotis bl

--- LibMaple/support/make/board-includes/maple_mini.mk
+++ LibMaple/support/make/board-includes/maple_mini.mk
@@ -4,4 +4,9 @@ ERROR_LED_PORT := GPIOB
 ERROR_LED_PIN  := 1
 MCU_SERIES := stm32f1
 MCU_F1_LINE := performance
+ifeq ($(BOOTLOADER),maple)
 LD_MEM_DIR := sram_20k_flash_128k
+endif
+ifeq ($(BOOTLOADER),robotis)
+LD_MEM_DIR := sram_20k_flash_128k_robotis
+endif
